c9d4a86adceae7ee99a3d21f72b3a38613b1fde9
admin <62morozov.dima.90@gmail.com>
2023-04-15 15:20:21 +0300

    HomeWork_Stream_2.8

--- src/main/java/HomeWork/Employee.java
+++ src/main/java/HomeWork/Employee.java
@@ -1,26 +1,62 @@
 package HomeWork;
 
 
+
+
 import java.util.Objects;
+
 
 public class Employee {
 
+
     private final String name;
+
 
     private final String surname;
 
-    public Employee(String name, String surname) {
+
+    private final double salary;
+
+
+    private final int department;
+
+
+
+
+    public Employee(String name, String surname, double salary, int department) {
         this.name = name;
         this.surname = surname;
+
+
+        this.salary = salary;
+        this.department = department;
     }
+
+
+    public double getSalary() {
+        return salary;
+    }
+
+
+    public int getDepartment() {
+        return department;
+    }
+
 
     public String getName() {
         return name;
     }
 
+
     public String getSurname() {
         return surname;
     }
+
+
+    public String getNameKey() {
+        return name + " " + surname;
+    }
+
 
     @Override
     public boolean equals(Object o) {
@@ -34,14 +70,17 @@ public class Employee {
         return name.equals(employee.name) && surname.equals(employee.surname);
     }
 
+
     @Override
     public int hashCode() {
         return Objects.hash(name, surname);
     }
 
+
     @Override
     public String toString() {
         return surname + " " + name;
     }
+
 
 }
---
+++ src/main/java/HomeWork/controller/EmployeeController.java
@@ -0,0 +1,51 @@
+package HomeWork.controller;
+import HomeWork.Employee;
+import HomeWork.service.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+
+    @GetMapping("/add")
+    public Employee add(@RequestParam String firstName,
+                        @RequestParam String lastName,
+                        @RequestParam Double salary,
+                        @RequestParam Integer department) {
+        return employeeService.addEmployee(firstName, lastName,salary ,department);
+    }
+
+
+    @GetMapping("/remove")
+    public Employee remove(@RequestParam String firstName,
+                           @RequestParam String lastName,
+                           @RequestParam Double salary,
+                           @RequestParam Integer department) {
+        return employeeService.removeEmployee(firstName, lastName, salary, department);
+    }
+
+
+    @GetMapping("/find")
+    public Employee find(@RequestParam String firstName,
+                         @RequestParam String lastName,
+                         @RequestParam Double salary,
+                         @RequestParam Integer department) {
+        return employeeService.findEmployee(firstName, lastName, salary, department);
+    }
+
+
+    @GetMapping
+    public Collection<Employee> list() {
+        return employeeService.list();
+    }
+}
